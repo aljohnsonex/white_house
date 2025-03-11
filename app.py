2bdc00b0347063e8d009b6ec584f352819a0bead
Alex Johnson <alexjohnson@alexs-mbp.mynetworksettings.com>
2025-03-10 21:07:29 -0400

    github

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ import base64
 
 # st.write(st.secrets) 
 
-hide_github_icon = """
-<style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-</style>
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+# hide_github_icon = """
+# <style>
+# #MainMenu {visibility: hidden;}
+# footer {visibility: hidden;}
+# </style>
+# """
+# st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 try:
     # Initialize BigQuery client
